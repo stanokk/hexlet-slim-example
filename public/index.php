b1e6dccfbcb4537cf4760f1dbc2880ed756b2666
Evgeny Kuzin <ekuzin@outlook.com>
2022-04-15 13:55:59 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,17 +17,10 @@ $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
-    // $this в Slim это контейнер зависимостей
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
-
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
+
 $app->get('/users', function ($request, $response) use ($users) {
     $name = $request->getQueryParam('name');
     $filteredNames = array_filter($users, fn($item) => str_contains($item, $name) ? $item : null); 
@@ -35,13 +28,38 @@ $app->get('/users', function ($request, $response) use ($users) {
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
+$app->get('/users/new', function ($request, $response) {
+    $id = uniqid();
+    $params = [
+	    'user' => ['id' => $id, 'nickname' => '', 'email' => '']
+   ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-'. $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
+});
+
+$app->post('/users', function ($request, $response) {
+    $user = $request->getParsedBody();
+    var_dump($user);
+    $file = 'people.json';
+    $current = file_get_contents($file);
+    empty($current) ? $updated = [] : $updated = json_decode($current, true);
+    $newUser = ($user);
+    $updated[] =  $newUser;
+    var_dump($updated);
+    file_put_contents($file, json_encode($updated));
+    $params = [
+        'user' => $user 
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
 });
 
 $app->run();
